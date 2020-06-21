df1569d5273c7bb2373a2a76a726ad1e24d30341
Michal <michal.danieluk@gmail.com>
2020-06-21 20:35:23 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+
+function zbierzIloscPaktietow () {
+
+ 
+    const pakietBezDoplatyPodstawowyIndywidualny = document.getElementById("pakietBezDoplatyPodstawowyIndywidualny").value
+    const pakietZDoplataPodstawowyIndywidualny = document.getElementById("pakietZDoplataPodstawowyIndywidualny").value
+    const pakietBezDoplatyPodstawowyRodzinny = document.getElementById("pakietBezDoplatyPodstawowyRodzinny").value
+    const pakietZDoplataPodstawowyRodzinny = document.getElementById("pakietZDoplataPodstawowyRodzinny").value
+    const pakietBezDoplatyRozszerzonyIndywidualny = document.getElementById("pakietBezDoplatyRozszerzonyIndywidualny").value
+    const pakietZDoplataRozszerzonyIndywidualny = document.getElementById("pakietBezDoplatyRozszerzonyIndywidualny").value
+    const pakietBezDoplatyRozszerzonyRodzinny = document.getElementById("pakietBezDoplatyRozszerzonyIndywidualny").value
+    const pakietZDoplataRozszerzonyRodzinny = document.getElementById("pakietBezDoplatyRozszerzonyIndywidualny").value
+    const pakietBezDoplatyKompleksowyIndywidualny = document.getElementById("pakietBezDoplatyRozszerzonyIndywidualny").value
+    const pakietBezDoplatyKompleksowyRodzinny = document.getElementById("pakietBezDoplatyRozszerzonyIndywidualny").value
+
+
+    // if (condition) {
+        
+    // }
+
+    console.log(pakietBezDoplatyPodstawowyIndywidualny, pakietZDoplataPodstawowyIndywidualny);
+
+}
